6eb74c550a3575a9fe855c06982b656bbf41ec53
toxypiks <HannaHinkelbein89@gmx.de>
2024-12-06 18:10:11 +0100

    Print all character events from xml

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::process::exit;
-use xml::reader::EventReader;
+use xml::reader::{EventReader, XmlEvent};
 
 fn main() {
     let file_path = "./docs.gl/gl4/glClear.xhtml";
@@ -8,11 +8,17 @@ fn main() {
     // returns result -> result has unwrap or else function so throw custom error
     let file = File::open("./docs.gl/gl4/glClear.xhtml").unwrap_or_else(|err| {
         eprintln!("ERROR: could not read file {file_path}: {err}");
-        exit(1)
+        exit(1);
     });
     let er = EventReader::new(file);
 
     for event in er.into_iter() {
-        println!("{event:?}")
+        let event = event.unwrap_or_else(|err| {
+            eprintln!("ERROR: could not read next XML event: {err}");
+            exit(1);
+        });
+        if let XmlEvent::Characters(text) = event {
+            println!("{text}");
+        }
     }
 }
