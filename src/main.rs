75768c76f492906551516bb37edadf0b12936cbb
toxypiks <HannaHinkelbein89@gmx.de>
2024-12-06 17:11:10 +0100

    Read from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "./docs.gl/gl4/glClear.xhtml";
+    // returns result -> result has unwrap or else function so throw custom error
+    let content = fs::read_to_string(file_path).unwrap_or_else(|err| {
+        eprintln!("ERROR: could not read file {file_path}: {err}");
+        exit(1)
+    });
+    println!("{content}");
 }
